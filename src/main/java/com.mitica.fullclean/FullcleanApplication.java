5853376798c7a68b800c7dd245e07475b5753976
DevCordeirocf <luiscordeiro2006@gmail.com>
2025-11-27 14:40:02 -0300

    1conexão bd

--- src/main/java/com.mitica.fullclean/FullcleanApplication.java
+++ src/main/java/com.mitica.fullclean/FullcleanApplication.java
@@ -2,6 +2,8 @@ package com.mitica.fullclean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.experimental.SuperBuilder;
+
 
 @SpringBootApplication
 public class FullcleanApplication {
@@ -9,5 +11,7 @@ public class FullcleanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FullcleanApplication.class, args);
 	}
+
+	public final class TenantContext { private static final ThreadLocal<String> CURRENT_TENANT = new ThreadLocal<>(); private TenantContext() {} 
 
 }
