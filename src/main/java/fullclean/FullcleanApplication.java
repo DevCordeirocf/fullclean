0a095d51162318fa98ef689f5f5708045b6ad0e3
DevCordeirocf <luiscordeiro2006@gmail.com>
2025-12-02 10:52:45 -0300

    Primeiro commit

---
+++ src/main/java/fullclean/FullcleanApplication.java
@@ -0,0 +1,13 @@
+package fullclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullcleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullcleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fullclean/FullcleanApplicationTests.java
@@ -0,0 +1,13 @@
+package fullclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullcleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
