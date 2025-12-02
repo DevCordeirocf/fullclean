ba5897478946986775eb98b893c79d8c6e365149
DevCordeirocf <luiscordeiro2006@gmail.com>
2025-12-02 19:59:04 -0300

    fix: import in testeTenantRepository.java

--- src/main/java/fullclean/repository/TesteTenantRepository.java
+++ src/main/java/fullclean/repository/TesteTenantRepository.java
@@ -1,6 +1,9 @@
 package fullclean.repository;
 
 import fullclean.entity.TesteTenant;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
